5f3c569d02a9114ad0c1f2cd5bf26317d2f0d87b
Freshwind01 <90686749+Freshwind01@users.noreply.github.com>
2026-04-18 09:50:45 +0300

    Update index.js

--- 01-arrays-objects/index.js
+++ 01-arrays-objects/index.js
@@ -1,1 +1,11 @@
+// Входные данные от API
+const apiUsers = [
+  { id: 1, first_name: 'John', last_name: 'Doe', email: 'john@example.com', is_active: true },
+  { id: 2, first_name: 'Jane', last_name: 'Smith', email: 'jane@example.com', is_active: false },
+];
 
+// Нужно получить:
+// [
+//   { id: 1, fullName: 'John Doe', email: 'john@example.com', active: true },
+//   { id: 2, fullName: 'Jane Smith', email: 'jane@example.com', active: false },
+// ]
