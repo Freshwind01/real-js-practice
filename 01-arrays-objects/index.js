4d5ef448178b3a8a471ee63c062846b6feabd7fd
Freshwind01 <90686749+Freshwind01@users.noreply.github.com>
2026-04-18 22:58:33 +0300

    Update index.js

--- 01-arrays-objects/index.js
+++ 01-arrays-objects/index.js
@@ -9,3 +9,11 @@ const apiUsers = [
 //   { id: 1, fullName: 'John Doe', email: 'john@example.com', active: true },
 //   { id: 2, fullName: 'Jane Smith', email: 'jane@example.com', active: false },
 // ]
+function TransformUser(users) {
+  return users.map(user => ({
+    id: user.id,
+    fullName: user.first_name + ' ' + user.last_name,
+    email: user.email,
+    active: user.is_active,
+  }));
+}
