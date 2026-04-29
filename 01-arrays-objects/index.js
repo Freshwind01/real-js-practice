525982205469ac27c1bb8cd4d74849ab74a8a453
Freshwind01 <90686749+Freshwind01@users.noreply.github.com>
2026-04-29 19:38:24 +0300

    Update index.js

--- 01-arrays-objects/index.js
+++ 01-arrays-objects/index.js
@@ -1,3 +1,4 @@
+//Задача 1.1
 // Входные данные от API
 const apiUsers = [
   { id: 1, first_name: 'John', last_name: 'Doe', email: 'john@example.com', is_active: true },
@@ -9,6 +10,7 @@ const apiUsers = [
 //   { id: 1, fullName: 'John Doe', email: 'john@example.com', active: true },
 //   { id: 2, fullName: 'Jane Smith', email: 'jane@example.com', active: false },
 // ]
+//Решение:
 function TransformUser(users) {
   return users.map(user => ({
     id: user.id,
@@ -17,3 +19,30 @@ function TransformUser(users) {
     active: user.is_active,
   }));
 }
+
+## Задача 1.2: Группировка товаров по категориям 🟢
+**Контекст:** У вас есть список товаров, нужно сгруппировать их по категориям для отображения в UI.
+**Задача:**
+const products = [
+  { id: 1, name: 'Laptop', category: 'Electronics', price: 1000 },
+  { id: 2, name: 'Shirt', category: 'Clothing', price: 50 },
+  { id: 3, name: 'Phone', category: 'Electronics', price: 800 },
+  { id: 4, name: 'Jeans', category: 'Clothing', price: 80 },
+];
+
+// Нужно получить:
+// {
+//   Electronics: [{ id: 1, name: 'Laptop', price: 1000 }, { id: 3, name: 'Phone', price: 800 }],
+//   Clothing: [{ id: 2, name: 'Shirt', price: 50 }, { id: 4, name: 'Jeans', price: 80 }]
+// }
+
+//Решение:
+function groupCat(products) {
+  return products.reduce((acc,currentObject) => {
+  const key =currentObject.category;
+    if (!acc[key]) {acc[key]=[];}
+    delete currentObject.category;
+    acc[key].push(currentObject);
+    return acc;
+                                                },{})
+                            }
